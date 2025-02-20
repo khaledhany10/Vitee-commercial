9052199a67743d8d9d9c73d1ebda261b95f65214
khaledhany10 <hanykhaled153@gmail.com>
2025-02-20 12:00:51 +0200

    Fixed Pages GitHub

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Vitee-commercial/",
-  server:{
+  base: "/Vitee-commercial/",
+  server: {
     port: 5173
   }
 })
-
-
-
-
-
